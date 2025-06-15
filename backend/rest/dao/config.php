1175a5a9edc941c8265d84320ed23878c8d4f5c1
mirzacesir <mirza.cesir@stu.ibu.edu.ba>
2025-06-15 20:48:29 +0200

    Change

--- backend/rest/dao/config.php
+++ backend/rest/dao/config.php
@@ -11,29 +11,33 @@ class Config
 {
    public static function DB_NAME()
    {
-       return 'parfumes'; 
+       return Config::get_env("DB_NAME", "parfumes"); 
    }
    public static function DB_PORT()
    {
-       return  3306;
+       return Config::get_env("DB_PORT", 3306);
    }
    public static function DB_USER()
    {
-       return 'root';
+       return Config::get_env("DB_USER", 'root');
    }
    public static function DB_PASSWORD()
    {
-       return '0000';
+       return Config::get_env("DB_PASSWORD", '0000');
    }
    public static function DB_HOST()
    {
-       return '127.0.0.1';
+       return Config::get_env("DB_HOST", '127.0.0.1');
    }
-
 
    public static function JWT_SECRET() {
-       return 'mileLegenda333';
+       return Config::get_env("JWT_SECRET", 'mileLegenda333');
    }
+
+   public static function get_env($name, $default){
+       return isset($_ENV[$name]) && trim($_ENV[$name]) != "" ? $_ENV[$name] : $default;
+   }
+
 }
 
 class Database {
